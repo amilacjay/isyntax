510ac1e2a2c185012062c39616e5f2a7629ec820
Iloshini <iloshinikarunarathne@outlook.com>
2017-06-11 11:27:57 +0530

    Edited.

--- dbcreator/test/test2.py
+++ dbcreator/test/test2.py
@@ -2,17 +2,20 @@ from nltk.stem import WordNetLemmatizer
 
 wnl = WordNetLemmatizer()
 
-def isplural(word):
-    lemma = wnl.lemmatize(word, 'n')
-    plural = True if word is not lemma else False
-    return plural, lemma
+# def isplural(word):
+#     lemma = wnl.lemmatize(word, 'n')
+#     plural = True if word is not lemma else False
+#     return plural, lemma
+#
+# # nounls = ['geese', 'mice', 'bars', 'foos', 'foo',
+# #                 'families', 'family', 'dog', 'dogs']
+#
+# nounls = ['employee', 'employees', 'doctor', 'doctors']
+#
+# for nn in nounls:
+#     isp, lemma = isplural(nn)
+#     print (nn, lemma, isp)
 
-# nounls = ['geese', 'mice', 'bars', 'foos', 'foo',
-#                 'families', 'family', 'dog', 'dogs']
 
-nounls = ['employee', 'employees', 'doctor', 'doctors']
-
-for nn in nounls:
-    isp, lemma = isplural(nn)
-    print (nn, lemma, isp)
-
+lemma = wnl.lemmatize('drugs')
+print(lemma)
